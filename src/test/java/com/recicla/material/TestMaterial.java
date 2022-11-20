4339a23fbdbfc5b81dca8373b6c9510f057fbe2b
Gabriel Bonamico <61624491+bonamicojedi@users.noreply.github.com>
2022-11-19 23:57:22 -0300

    Create TestMaterial.java

---
+++ src/test/java/com/recicla/material/TestMaterial.java
@@ -0,0 +1,77 @@
+package com.recicla.material;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.recicla.material.controller.ControllerMaterial;
+import com.recicla.material.model.bean.Material;
+
+import junit.framework.TestCase;
+
+
+public class TestMaterial extends TestCase {
+
+	@Test
+	@DisplayName("Teste de inserção de Material")
+	public void testInserirMaterial() throws ClassNotFoundException, SQLException, ParseException {
+
+		Material newCol = new Material(3,"latinha de cerveja","descricao");
+
+		ControllerMaterial contCol = new ControllerMaterial();
+		assertNotNull("nao encontrado",contCol.inserir(newCol).getId());
+		assertNotNull("nao encontrado",contCol.inserir(newCol).getId());
+		assertNotNull("nao encontrado",contCol.inserir(newCol).getId());
+		assertNotNull("nao encontrado",contCol.inserir(newCol).getId());
+
+
+	}
+
+	@Test
+	@DisplayName("Teste de busca de Material")
+	public void testBuscarMaterial() throws ClassNotFoundException, SQLException, ParseException {
+		Material newCol = new Material(3);
+		ControllerMaterial contCol = new ControllerMaterial();
+
+		newCol=(contCol.buscar(newCol));
+        System.out.println(newCol);
+	}
+
+	 @Test
+	 @DisplayName("Teste de listar de Material")
+	 public void testListarMaterial() throws ClassNotFoundException, SQLException, ParseException {
+
+	 	Material newCol = new Material("latinha de cerveja");
+	 	ControllerMaterial contCol = new ControllerMaterial();
+	 	assertNotNull(contCol.listar(newCol));
+        System.out.println(newCol);
+	 }
+
+	// @Test
+	// @DisplayName("Teste de excluir de Material")
+	// public void testExcluirMaterial() throws ClassNotFoundException, SQLException, ParseException {
+	// 	Material newCol = new Material(1);
+	// 	ControllerMaterial contCol = new ControllerMaterial();
+
+	// 	assertNotNull(contCol.excluir(newCol));
+	// }
+
+	// @Test
+	// @DisplayName("Teste de alterar de Material")
+	// public void testAlterarMaterial() throws ClassNotFoundException, SQLException, ParseException {
+	// 	SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+	// 	Date dataSol = new Date();
+	// 	Date dataCon = formatter.parse("30-11-2022");
+	// 	Material newCol = new Material(1, 1, "Bloco B", 3, new java.sql.Date(dataSol.getTime()),
+	// 			new java.sql.Date(dataCon.getTime()), 3, 4, 3, 4);
+
+	// 	ControllerMaterial contCol = new ControllerMaterial();
+	// 	assertNotNull(contCol.alterar(newCol).getId());
+
+	// }
+
+}
