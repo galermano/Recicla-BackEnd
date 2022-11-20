bbcb8fd5cc4007f9cbd47cbf6e6a29c1339c6fae
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2022-11-20 13:32:35 -0300

    Adição de instâncias

--- src/main/java/com/recicla/contAcesso/model/bean/HistoricoAcesso.java
+++ src/main/java/com/recicla/contAcesso/model/bean/HistoricoAcesso.java
@@ -5,8 +5,11 @@ import java.util.Date;
 public class HistoricoAcesso {
 	private Integer id;
 	private Integer id_usu;
+	private Integer id_mod;
+	private Integer id_usu_mod;
+	private Integer tipo;
 	private Date dataAcesso;
-	
+
 	public HistoricoAcesso() {
 	}
 
@@ -57,9 +60,33 @@ public class HistoricoAcesso {
 		this.dataAcesso = dataAcesso;
 	}
 
+	public Integer getId_mod() {
+		return id_mod;
+	}
+
+	public void setId_mod(Integer id_mod) {
+		this.id_mod = id_mod;
+	}
+		
+	public Integer getAcesso() {
+		return id_usu_mod;
+	}
+
+	public void setAcesso(Integer id_usu_mod) {
+		this.id_usu_mod = id_usu_mod;
+	}
+	
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+
 	@Override
 	public String toString() {
-		return "HistoricoAcesso [id=" + id + ", id_usu=" + id_usu + ", dataAcesso=" + dataAcesso + "]";
+		return "HistoricoAcesso [id=" + id + ", id_usu=" + id_usu + ", dataAcesso=" + dataAcesso + ", id_mod=" + id_mod + ", id_usu_mod=" + id_usu_mod + ", tipo=" + tipo + "]";
 	}
 
 }
