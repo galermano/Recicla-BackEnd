a86e5875342d3889906eece820511a0f90826e82
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2022-11-20 16:01:49 -0300

    Correção Bean Historico acesso

--- src/main/java/com/recicla/contAcesso/model/bean/HistoricoAcesso.java
+++ src/main/java/com/recicla/contAcesso/model/bean/HistoricoAcesso.java
@@ -1,63 +1,33 @@
 package com.recicla.contAcesso.model.bean;
 
-import java.util.Date;
-
 public class HistoricoAcesso {
-	private Integer id;
 	private Integer id_usu;
 	private Integer id_mod;
 	private Integer id_usu_mod;
+
 	private Integer tipo;
-	private Date dataAcesso;
 
 	public HistoricoAcesso() {
 	}
 
-	public HistoricoAcesso(Integer id, Integer id_usu, Date dataAcesso) {
-		super();
-		this.id = id;
-		this.id_usu = id_usu;
-		this.dataAcesso = dataAcesso;
-	}
-
-	public HistoricoAcesso(Integer id_usu, Date dataAcesso) {
+	public HistoricoAcesso(Integer id_usu, Integer id_mod, Integer id_usu_mod, Integer tipo) {
 		super();
 		this.id_usu = id_usu;
-		this.dataAcesso = dataAcesso;
-	}
-	
-	public HistoricoAcesso(Integer id) {
-		super();
-		this.id = id;
-	}
-
-	public HistoricoAcesso(Date dataAcesso) {
-		super();
-		this.dataAcesso = dataAcesso;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
+		this.id_mod = id_mod;
+		this.id_usu_mod = id_usu_mod;
+		this.tipo = tipo;
 	}
 
 	public Integer getId_usu() {
 		return id_usu;
 	}
 
-	public void setId_usu(Integer id_usu) {
+	public HistoricoAcesso(Integer id_usu) {
 		this.id_usu = id_usu;
 	}
 
-	public Date getDataAcesso() {
-		return dataAcesso;
-	}
-
-	public void setDataAcesso(Date dataAcesso) {
-		this.dataAcesso = dataAcesso;
+	public void setId_usu(Integer id_usu) {
+		this.id_usu = id_usu;
 	}
 
 	public Integer getId_mod() {
@@ -67,7 +37,7 @@ public class HistoricoAcesso {
 	public void setId_mod(Integer id_mod) {
 		this.id_mod = id_mod;
 	}
-		
+
 	public Integer getAcesso() {
 		return id_usu_mod;
 	}
@@ -75,7 +45,7 @@ public class HistoricoAcesso {
 	public void setAcesso(Integer id_usu_mod) {
 		this.id_usu_mod = id_usu_mod;
 	}
-	
+
 	public Integer getTipo() {
 		return tipo;
 	}
@@ -84,9 +54,18 @@ public class HistoricoAcesso {
 		this.tipo = tipo;
 	}
 
+	public Integer getId_usu_mod() {
+		return id_usu_mod;
+	}
+
+	public void setId_usu_mod(Integer id_usu_mod) {
+		this.id_usu_mod = id_usu_mod;
+	}
+
 	@Override
 	public String toString() {
-		return "HistoricoAcesso [id=" + id + ", id_usu=" + id_usu + ", dataAcesso=" + dataAcesso + ", id_mod=" + id_mod + ", id_usu_mod=" + id_usu_mod + ", tipo=" + tipo + "]";
+		return "HistoricoAcesso [, id_usu=" + id_usu + ", id_mod=" + id_mod
+				+ ", id_usu_mod=" + id_usu_mod + ", tipo=" + tipo + "]";
 	}
 
 }
