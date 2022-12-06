7807ef023b6a214e3785f99dfac04885104f4ac0
VictorCaique <vcds4747@gmail.com>
2022-12-06 12:11:40 -0300

    Fixing Coleta

--- src/main/java/com/recicla/coleta/model/bean/Coleta.java
+++ src/main/java/com/recicla/coleta/model/bean/Coleta.java
@@ -1,17 +1,16 @@
 package com.recicla.coleta.model.bean;
 
 import com.recicla.contAcesso.model.bean.Usuario;
-<<<<<<< Updated upstream
+
 import com.recicla.util.model.bean.Status;
-=======
+
 import com.recicla.material.model.bean.Material;
 
->>>>>>> Stashed changes
+
 import java.sql.Date;
 
 public class Coleta {
 
-<<<<<<< Updated upstream
     private int id;
     private int idStatus;
     private Status status;
@@ -196,70 +195,9 @@ public class Coleta {
     public void setIdUsuarioE(int idUsuarioE) {
         this.idUsuarioE = idUsuarioE;
     }
-=======
-	private int id;
-	private int idStatus;
-	private String compl;
-	private int quantidade;
-	private Date dataSol;
-	private Date dataCon;
-	//Id de logradouro de coleta (daonde pega)
-	private int idLogradouroR;
-	private Logradouro logradouroR;
-	//Id de logradouro de entrega (daonde entrega)
-	private int idLogradouroE;
-	private Logradouro logradouroE;
-	//Id de Usuario  de entrega (daonde entrega)
-	private int idUsuarioR;
-	private Usuario usuarioR;
-	//Id de Usuario  de entrega (daonde entrega)
-	private int idUsuarioE;
-	private Usuario usuarioE;
+    
 	private int idMaterial;
 	private Material material;
-
-	
-	
-	public Coleta(int idStatus, String compl, int quantidade, Date dataSol, int idLogradouroR, int idLogradouroE,
-			int idUsuarioR, int idUsuarioE) {
-		super();
-		this.idStatus = idStatus;
-		this.compl = compl;
-		this.quantidade = quantidade;
-		this.dataSol = dataSol;
-		this.idLogradouroR = idLogradouroR;
-		this.idLogradouroE = idLogradouroE;
-		this.idUsuarioR = idUsuarioR;
-		this.idUsuarioE = idUsuarioE;
-	}
-
-	public Coleta(int id, int idStatus, String compl, int quantidade, Date dataSol, Date dataCon, int idLogradouroR,
-			int idLogradouroE, int idUsuarioR, int idUsuarioE) {
-		this.id = id;
-		this.idStatus = idStatus;
-		this.compl = compl;
-		this.quantidade = quantidade;
-		this.dataSol = dataSol;
-		this.dataCon = dataCon;
-		this.idLogradouroR = idLogradouroR;
-		this.idLogradouroE = idLogradouroE;
-		this.idUsuarioR = idUsuarioR;
-		this.idUsuarioE = idUsuarioE;
-	}
-
-	public Coleta(int idStatus, String compl, int quantidade, Date dataSol, Date dataCon, int idLogradouroR,
-			int idLogradouroE, int idUsuarioR, int idUsuarioE) {
-		this.idStatus = idStatus;
-		this.compl = compl;
-		this.quantidade = quantidade;
-		this.dataSol = dataSol;
-		this.dataCon = dataCon;
-		this.idLogradouroR = idLogradouroR;
-		this.idLogradouroE = idLogradouroE;
-		this.idUsuarioR = idUsuarioR;
-		this.idUsuarioE = idUsuarioE;
-	}
-	
 	
 
 	public Coleta(int id, int idStatus, String compl, int quantidade, Date dataSol, Date dataCon, int idLogradouroR,
@@ -300,132 +238,6 @@ public class Coleta {
 		this.material = material;
 	}
 
-	@Override
-	public String toString() {
-		return "Coleta [id=" + id + ", idStatus=" + idStatus + ", compl=" + compl + ", quantidade=" + quantidade
-				+ ", dataSol=" + dataSol + ", dataCon=" + dataCon + ", idLogradouroR=" + idLogradouroR
-				+ ", logradouroR=" + logradouroR + ", idLogradouroE=" + idLogradouroE + ", logradouroE=" + logradouroE
-				+ ", idUsuarioR=" + idUsuarioR + ", usuarioR=" + usuarioR + ", idUsuarioE=" + idUsuarioE + ", usuarioE="
-				+ usuarioE + "]";
-	}
-
-	public Coleta(int idStatus, String compl, int quantidade, Date dataSol, Date dataCon, int idLogradouroR,
-	Logradouro logradouroR, int idLogradouroE, Logradouro logradouroE, int idUsuarioR, int idUsuarioE, Usuario usuarioR, Usuario usuarioE) {
-		this.idStatus = idStatus;
-		this.compl = compl;
-		this.quantidade = quantidade;
-		this.dataSol = dataSol;
-		this.dataCon = dataCon;
-		this.idLogradouroR = idLogradouroR;
-		this.logradouroR = logradouroR;
-		this.idLogradouroE = idLogradouroE;
-		this.logradouroE = logradouroE;
-		this.idUsuarioR = idUsuarioR;
-		this.usuarioR = usuarioR;
-		this.idUsuarioE = idUsuarioE;
-		this.usuarioE = usuarioE;
-	}
-
-	public Coleta(Date dataSol) {
-		this.dataSol = dataSol;
-	}
-	
-	public Coleta(int id) {
-		this.id = id;
-	}
-	
-	
-	public Coleta(int id, String compl, int quantidade, Date dataSol, Date dataCon) {
-		this.id = id;
-		this.compl = compl;
-		this.quantidade = quantidade;
-		this.dataSol = dataSol;
-		this.dataCon = dataCon;
-	}
-
-	public Coleta() {
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getIdStatus() {
-		return idStatus;
-	}
-
-	public void setIdStatus(int idStatus) {
-		this.idStatus = idStatus;
-	}
-
-	public String getCompl() {
-		return compl;
-	}
-
-	public void setCompl(String compl) {
-		this.compl = compl;
-	}
-
-	public int getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	public Date getDataSol() {
-		return dataSol;
-	}
-
-	public void setDataSol(Date dataSol) {
-		this.dataSol = dataSol;
-	}
-
-	public Date getDataCon() {
-		return dataCon;
-	}
-
-	public void setDataCon(Date dataCon) {
-		this.dataCon = dataCon;
-	}
-
-	public int getIdLogradouroR() {
-		return idLogradouroR;
-	}
-
-	public void setIdLogradouroR(int idLogradouroR) {
-		this.idLogradouroR = idLogradouroR;
-	}
-
-	public int getIdLogradouroE() {
-		return idLogradouroE;
-	}
-
-	public void setIdLogradouroE(int idLogradouroE) {
-		this.idLogradouroE = idLogradouroE;
-	}
-
-	public int getIdUsuarioR() {
-		return idUsuarioR;
-	}
-
-	public void setIdUsuarioR(int idUsuarioR) {
-		this.idUsuarioR = idUsuarioR;
-	}
-
-	public int getIdUsuarioE() {
-		return idUsuarioE;
-	}
-
-	public void setIdUsuarioE(int idUsuarioE) {
-		this.idUsuarioE = idUsuarioE;
-	}
-
 	public Logradouro getLogradouroR() {
 		return logradouroR;
 	}
@@ -441,23 +253,6 @@ public class Coleta {
 	public void setLogradouroE(Logradouro logradouroE) {
 		this.logradouroE = logradouroE;
 	}
->>>>>>> Stashed changes
-
-    public Logradouro getLogradouroR() {
-        return logradouroR;
-    }
-
-    public void setLogradouroR(Logradouro logradouroR) {
-        this.logradouroR = logradouroR;
-    }
-
-    public Logradouro getLogradouroE() {
-        return logradouroE;
-    }
-
-    public void setLogradouroE(Logradouro logradouroE) {
-        this.logradouroE = logradouroE;
-    }
 
     public Usuario getUsuarioR() {
         return usuarioR;
@@ -483,12 +278,6 @@ public class Coleta {
         this.status = status;
     }
 
-<<<<<<< Updated upstream
-=======
-	public void setUsuarioE(Usuario usuarioE) {
-		this.usuarioE = usuarioE;
-	}
-
 	public int getIdMaterial() {
 		return idMaterial;
 	}
@@ -506,5 +295,4 @@ public class Coleta {
 	}
 	
 	
->>>>>>> Stashed changes
 }
